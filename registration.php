32b8e9860b310673383e4a82cfd240008f78407f
Adewale <jamiuadewaleyusuf@gmail.com>
2024-02-14 23:28:09 +0100

    updating

--- registration.php
+++ registration.php
@@ -7,18 +7,18 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
     $phone = $_POST["phone-number"];
 
     // Prepare email content
-    $to = "adewale@afriwoodstreams.com"; // Your email address
+    $to = "adewale@afriwoodstreams.com"; 
     $subject = "New Registration";
     $message = "Name: $name\nEmail: $email\nPhone Number: $phone";
 
     // Send email
     $headers = "From: $email"; // Set the sender's email address
     if (mail($to, $subject, $message, $headers)) {
-        // Email sent successfully
-        echo "Thank you for registering, $name! Your registration details have been sent to adewale@afriwoodstreams.com.";
-    } else {
-        // Error sending email
-        echo "Oops! Something went wrong. Please try again later.";
-    }
+         // Email sent successfully
+         echo '<script>alert("Thank you for registering, ' . $name . '! We\'ll get in touch with you shortly.");</script>';
+        } else {
+            // Error sending email
+            echo '<script>alert("Oops! Something went wrong. Please try again later.");</script>';
+        }
 }
 ?>
--- script.js
+++ script.js
@@ -1,10 +1,39 @@
+var slideImage = document.getElementById("slider-img");
+var storeSliderImages = [
+    "images/afriwoodstrewams-audition1.png", 
+    "images/afriwoodstrewams-audition2.jpg",   
+    "images/afriwoodstrewams-audition3.jpg",
+    "images/afriwoodstrewams-audition4.jpg", 
+    "images/afriwoodstrewams-audition5.jpg",
+    "images/afriwoodstrewams-audition7.jpg",
+    "images/afriwoodstrewams-audition8.jpg",
+    "images/afriwoodstrewams-audition9.png"
+];
+
+var len = storeSliderImages.length;
+var i = 0;
+
+function slider() {
+    if (i > len - 1) {
+        i = 0;
+    }
+
+    slideImage.src = storeSliderImages[i];
+    i++;
+    setTimeout(slider, 5000); 
+}
+
+// Start the slider
+slider();
+
+
 
 // Set the date for the audition
 const auditionDate = new Date('2024-02-24T10:00:00').getTime();
 
 
 // Update the countdown every second
-const timer = setInterval(function() {
+  const timer = setInterval(function() {
   const now = new Date().getTime();
   const distance = auditionDate - now;
 
